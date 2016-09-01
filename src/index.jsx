687e57e247488fa93002a167dacf7d5902644681
Jorge Ivan Morales <jorge.iv.morales@gmail.com>
2016-08-31 21:31:33 -0500

    Flattened file structure

--- src/index.jsx
+++ src/index.jsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
-import App from './client/App';
-import reducers from './reducers';
+import App from './App';
+import reducers from './rootReducer';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
