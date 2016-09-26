f52654256102c047f4cb19ed44e7a8fa07b2d424
Jorge Ivan Morales <jorge.iv.morales@gmail.com>
2016-09-25 22:11:10 -0500

    File structure cleanup

--- src/index.jsx
+++ src/index.jsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
-import App from './App';
-import reducers from './rootReducer';
+import App from './app/App';
+import reducers from './app/rootReducer';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
